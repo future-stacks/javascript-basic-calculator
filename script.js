a1b8786893abd86412358ecf397d07b6175a4984
Future <futurehopesaviour@gmail.com>
2022-06-24 13:46:25 +0100

    submit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Scripting for Basic Calculator
+// Start with functions
+console.log(Start)
+function addition(num1, num2){
+    return num1+num2;
+}
+
+function subtraction(num1, num2){
+    return num1-num2;
+}
+
+function division(num1, num2){
+    return num1/num2;
+}
+
+function multiplication(num1, num2){
+    return num1*num2;
+}
+
+// Using IF statement
+let num1 = Number(prompt("Enter FIrst Number"));
+let num2 = Number(prompt("Enter Second Number"));
+// Define the prompts
+let choice = Number(prompt('What Operation would you \n 1. Addition \n 2. Subtraction \n 3. Division \n 4. Multiplication'));
+
+if (choice === 1){
+    console.log(addition(num1,num2));
+}
+else if (choice === 2){
+    console.log(subtraction(num1,num2));
+}
+else if (choice === 3){
+    console.log(division(num1,num2));
+}
+else if (choice === 4){
+    console.log(multiplication(num1,num2));
+}
+else{
+    console.log('Wrong input');
+}
+return end
